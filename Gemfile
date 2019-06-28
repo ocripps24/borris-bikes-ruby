c7b0ad22a949e9d531aca21c20c1adffdb327652
Oliver Cripps <ollycripps@gmail.com>
2019-06-28 11:18:42 +0100

    Initial commit with project setup

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+# frozen_string_literal: true
+
+gem 'rubocop', '0.56.0'
+
+group :test do
+  gem 'rspec'
+  gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
