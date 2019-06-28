31d73f99e3f9e0c0c7d9226c778fd63947fd7f71
Oliver Cripps <ollycripps@gmail.com>
2019-06-28 14:37:44 +0100

    Docks remember and report bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,12 +2,31 @@ require 'docking_station'
 
 describe DockingStation do
 
-  it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to(:release_bike) }
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
 
   describe '#release_bike' do
     it 'releases a working bike' do
       bike = subject.release_bike
       expect(bike).to be_working
+    end
+  end
+
+  describe '#dock' do
+    it 'docks a bike' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+  end
+
+  describe '#bike' do
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike 
     end
   end
 
